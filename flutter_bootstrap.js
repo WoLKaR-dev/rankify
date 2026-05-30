43c3666fb3b9baf6ad2350ba36291045b7758395
WoLKaR-dev <WoLKaR-dev@users.noreply.github.com>
2026-05-30 14:13:47 +0000

    Deploying to web from @ WoLKaR-dev/rankify@c135178a0c5bf29400b478e9d21d449bb13bb635 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1526280323" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2683075969" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
